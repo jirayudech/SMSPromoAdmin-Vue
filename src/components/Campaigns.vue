b0671d51e94589eb517368ac440bcafea21c2179
jirayudech <jirayudech@gmail.com>
2018-01-18 10:18:10 +0700

    initial

---
+++ src/components/Campaigns.vue
@@ -0,0 +1,25 @@
+<template>
+          <v-card color="purple" class="white--text">
+              <v-container fluid grid-list-lg>
+                <v-layout row>
+                  <v-flex xs7>
+                    <div>
+                      <div class="headline">Halycon Days</div>
+                      <div>Ellie Goulding</div>
+                    </div>
+                  </v-flex>
+                  <v-flex xs5>
+                      <v-card-media
+                        src="https://3.bp.blogspot.com/-gxlw2Wk3C2A/WCSBudGi6BI/AAAAAAAACZ8/BsBfbaOU56Uu7uuihikAnl7rxmuIvkRHACLcB/s400/Daily-SMS-Collection-Text-Msgs-Messages-in-English.jpg"
+                        height="125px"
+                        contain
+                      ></v-card-media>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card>
+</template>
+
+<script>
+  export default {}
+</script>
---
+++ src/store/state.js
@@ -0,0 +1,6 @@
+export const state = {
+  appName: 'Campaigns Management System',
+  user: null,
+  error: null,
+  loading: false
+}
